76e171a962cd575ec5790715a52082d21c6a5158
dayalanirohit <rohitkumardayalani@gmail.com>
2025-09-16 19:32:15 +0530

    Add files via upload

---
+++ Zcoder/src/App.js
@@ -0,0 +1,40 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import EditorPage from './pages/EditorPage';
+import NotesEditor from './pages/NotesEditor'
+import { Toaster } from 'react-hot-toast';
+
+
+function App() {
+  return (
+    <>
+
+      <div>
+        <Toaster position='top-right' toastOptions={
+          {
+            success:{
+              theme:{
+                primary: '#4aed88',
+              }
+            }
+          }
+        }> 
+        
+        </Toaster>
+      </div>
+      <BrowserRouter>
+        <Routes>
+            <Route path='/' element ={<Home />}></Route>
+            <Route path="/editor/:roomId" element ={<EditorPage />}></Route>
+            <Route path="/noteseditor/:roomId" element ={<NotesEditor />}></Route>
+        </Routes>
+      </BrowserRouter>
+    
+    </>
+  );
+}
+
+export default App;
+
+     
